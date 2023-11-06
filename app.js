1e2722752866725de7df2fc5236de73ddd69de59
Rohan Kaushik <rohankaushik1@outlook.com>
2023-11-06 22:13:12 +0530

    Added logger

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ import path from "path"
 import { fileURLToPath } from 'url';
 import methodOverride from "method-override"
 import mongoose from "mongoose";
+import morgan from "morgan";
 import { Campground } from "./models/campground.js";
 
 const port = 3000
 const app = express();
 
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -27,13 +29,10 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, '/views'));
 
 app.get("/",(req,res) => {
-    console.log('got request on / GET')
     res.render('home')
 })
 
 app.post("/campground",async(req,res) => {
-    console.log("got request on /campground POST")
-    console.log(req.body)
     const newCamp = new Campground(req.body.campground);
     await newCamp.save();
     res.redirect(302,`/campground/${newCamp._id}`)
@@ -41,19 +40,16 @@ app.post("/campground",async(req,res) => {
 
 app.get("/campground",async(req,res) => {
     const campground = await Campground.find({})
-    console.log('got request on /campground GET');
     res.render('campgrounds/index' , { campground })
 });
 
 app.get('/campground/:id/edit',async(req,res) => {
     const {id} =req.params
     const campground = await Campground.findById(id) 
-    console.log("got request on /campground/:id/edit GET")
     res.render('campgrounds/edit', {campground})
 })
 
 app.get('/campground/new',(req,res) => {
-    console.log("got request on /campground/new GET")
     res.render('campgrounds/new')
 })
 
@@ -64,14 +60,12 @@ app.get("/campground/:id",async(req,res) =>{
 })
 
 app.put("/campground/:id",async(req,res) => {
-    console.log("got request on /campground/:id PUT")
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, req.body, { new: true, runValidators: true })
     res.redirect(302, `/campground/${campground._id}`);
 })
 
 app.delete("/campground/:id", async (req, res) => {
-    console.log("got request on /campground/:id DELETE");
     const { id } = req.params;
     const deletedCamp = await Campground.findByIdAndDelete(id);
     console.log(`deleted ${JSON.stringify(deletedCamp)}`)
